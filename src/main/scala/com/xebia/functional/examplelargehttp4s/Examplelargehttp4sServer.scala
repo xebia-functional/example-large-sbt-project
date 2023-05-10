bbeeba64aa8da6a6cc95d491425071f122675f9b
Jack Viers <jackcviers@gmail.com>
2023-05-10 16:28:56 -0500

    Initial commit

---
+++ src/main/scala/com/xebia/functional/examplelargehttp4s/Examplelargehttp4sServer.scala
@@ -0,0 +1,38 @@
+package com.xebia.functional.examplelargehttp4s
+
+import cats.effect.Async
+import cats.syntax.all._
+import com.comcast.ip4s._
+import org.http4s.ember.client.EmberClientBuilder
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.implicits._
+import org.http4s.server.middleware.Logger
+
+object Examplelargehttp4sServer:
+
+  def run[F[_]: Async]: F[Nothing] = {
+    for {
+      client <- EmberClientBuilder.default[F].build
+      helloWorldAlg = HelloWorld.impl[F]
+      jokeAlg = Jokes.impl[F](client)
+
+      // Combine Service Routes into an HttpApp.
+      // Can also be done via a Router if you
+      // want to extract a segments not checked
+      // in the underlying routes.
+      httpApp = (
+        Examplelargehttp4sRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
+        Examplelargehttp4sRoutes.jokeRoutes[F](jokeAlg)
+      ).orNotFound
+
+      // With Middlewares in place
+      finalHttpApp = Logger.httpApp(true, true)(httpApp)
+
+      _ <- 
+        EmberServerBuilder.default[F]
+          .withHost(ipv4"0.0.0.0")
+          .withPort(port"8080")
+          .withHttpApp(finalHttpApp)
+          .build
+    } yield ()
+  }.useForever
